09586606622db85ecaa7971b6a16e09940a6ebb5
Nouhad17 <elhoubnouhad@gmail.com>
2025-10-09 11:42:36 +0100

    Fix: port serveur Express

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const bcrypt = require('bcryptjs');
 const db = require('./db');
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 // --- Middlewares ---
 app.use(cors());
