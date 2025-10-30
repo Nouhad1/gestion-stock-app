d5da3bf6fadfe09376e739a29841a1ab168ae500
Nouhad17 <elhoubnouhad@gmail.com>
2025-10-30 12:11:20 +0100

    Fix: restore gradlew for EAS Build

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { LogBox } from 'react-native'; // 👈 Import du LogBox
+import { LogBox } from 'react-native'; 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-import { registerRootComponent } from 'expo';
+// Tout d'abord, import nécessaire pour Reanimated (obligatoire)
+import 'react-native-reanimated';
 import 'react-native-gesture-handler';
 
+import { registerRootComponent } from 'expo';
 import App from './App';
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
+// Enregistrement du composant principal
 registerRootComponent(App);
-
