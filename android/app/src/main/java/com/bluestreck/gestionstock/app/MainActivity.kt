6c14e7c430ca2c09bcc85c038be2c0402153c11a
Nouhad17 <elhoubnouhad@gmail.com>
2025-10-30 09:34:14 +0100

    Fix: express pour commit

--- android/app/src/main/java/com/bluestreck/gestionstock/app/MainActivity.kt
+++ android/app/src/main/java/com/bluestreck/gestionstock/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.bluestreck.gestionstock
+package com.bluestreck.gestionstock.app
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/bluestreck/gestionstock/app/MainApplication.kt
+++ android/app/src/main/java/com/bluestreck/gestionstock/app/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.bluestreck.gestionstock
+package com.bluestreck.gestionstock.app
 
 import android.app.Application
 import android.content.res.Configuration
